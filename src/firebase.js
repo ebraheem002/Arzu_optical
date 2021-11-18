d0c19185dec83e055c3b27623c0ddf87ef29a381
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2021-11-18 12:49:46 +0300

    firebase conf

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCg2BpwgJha6T6TBkrinbwNjdYSZh2HbQ8",
+    authDomain: "arzu-optical.firebaseapp.com",
+    projectId: "arzu-optical",
+    storageBucket: "arzu-optical.appspot.com",
+    messagingSenderId: "163643703465",
+    appId: "1:163643703465:web:11fc209f68a5fff585e9b8",
+    measurementId: "G-E7D61QEBXC"
+  };
