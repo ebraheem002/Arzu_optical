6efec004dda41c719a8562a36a51bc33bbccc242
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2021-12-27 19:26:04 +0300

    added swiper to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './Header.js';
 import './App.css';
-import './Header.css'
-
+import './Header.css';
+import Swiper from './Swiper.js';
+import './Swiper.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Swiper/>
       <h1>hello world, how are you?</h1>
     </div>
   );
