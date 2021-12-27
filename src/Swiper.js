8907ff9b0ac12d8bb153b92ef3dd2a7ad772d936
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2021-12-27 19:25:52 +0300

    creating swiper files

---
+++ src/Swiper.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Swiper.css'
+
+function Swiper() {
+    return (
+<div className='swiper'>
+        
+        <div className='mySwiper'>
+            
+        </div>
+            
+</div>
+    );
+}
+
+export default Swiper
