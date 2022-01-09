50e77d7a3c9c23370ef724a4d6539fe6f53be48a
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2022-01-09 22:18:41 +0300

    creating home react component

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+function Home (){
+    return(
+
+        <div className='home_container'>
+            <div className='home_row'>
+                {myProduct}
+            </div>
+            <div className='home_row'>
+                {myProduct}
+                {myProduct}
+                {myProduct}
+            </div>
+            <div className='home_row'>
+                {myProduct}
+                {myProduct}
+            </div>
+            
+        </div>
+
+    )
+}
+
+export default Home
