aec7b654f4680816ce514215530329022d061743
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2022-01-23 15:26:51 +0300

    create product item

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Product.css';
 function Product(){
+    return(
     <div className='Product'>
-
+        <div className='product_item'>
+            <p>this is a sunglass</p>
+            <strong>99.99$</strong>
+        </div>
+        <img src='https://picsum.photos/200'/>
     </div>
+    )
 }
 
 export default Product
