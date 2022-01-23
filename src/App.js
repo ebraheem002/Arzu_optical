1226ffeef822779abfa96d0acd2eb7c36b3b3a1b
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2022-01-23 15:26:09 +0300

    add home to app js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Header from './Header.js';
 import Swiper from './Swiper.js';
-
+import Home from './Home.js';
 import './App.css';
 import './Header.css';
 import './Swiper.css';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Swiper/>
+      <Home />
       <h1>hello world, how are you?</h1>
     </div>
   );
