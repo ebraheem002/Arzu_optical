bf4042eee8250a52e68a5c85da50c23e29869a17
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2022-01-31 12:02:43 +0300

    added react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import React from 'react';
 import Header from './Header.js';
 import Swiper from './Swiper.js';
 import Home from './Home.js';
 import './App.css';
 import './Header.css';
 import './Swiper.css';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Header />
-      <Swiper/>
-      <Home />
-      <h1>hello world, how are you?</h1>
+      <Routes>
+        <Route path="/" element={<><Header/><Swiper/><Home/></>}>
+        </Route>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
