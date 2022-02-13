a59755e812255e68cd553920e15fe05a8e440b6e
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2022-02-13 18:35:50 +0300

    product page front

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Header from './Header.js';
 import Swiper from './Swiper.js';
 import Home from './Home.js';
+import ProductPage from './ProductPage'
 import './App.css';
 import './Header.css';
 import './Swiper.css';
+import './ProductPage.css'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 
 function App() {
@@ -14,8 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<><Header/><Swiper/><Home/></>}>
         </Route>
-        <Route path="/productPage" element={<><Header/><Swiper/><Home/></>}>
+        <Route path="/productPage" element={<><Header/><Swiper/><ProductPage/></>}>
         </Route>
+        
       </Routes>
       
     </div>
--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,8 +1,50 @@
 import React from "react";
 function ProductPage(){
     return(
-        <div className="container">
+        <div className="productContainer">
+            <div className="product-info">
+                <div className="productName">
+                    <h3>this is a glass</h3>
+                    <p>this is a good glass</p>
+                </div>
+                <div className="productPrice">
+                    9999
+                </div>
+                <div className="prod-colors">
+                    Colors: 
+                    <div className="each-color">
+
+                    </div>
+                    <div className="each-color">
+                        
+                    </div>
+                    <div className="each-color">
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="product-images">
+                <div className="product-images-side">
+                    <div className="product-image-div">
+                    <img className="mainProdctImg" src="https://picsum.photos/id/237/200/300"/>
+                    </div>
+                    <div className="product-images-swiper">
+                        <div className="slideimg">
+                            <img className="eachImg" src="https://picsum.photos/id/237/100/200"/>
+                        </div>
+                        <div className="slideimg">
+                            <img className="eachImg" src="https://picsum.photos/id/237/100/200"/>
+                        </div>
+                        <div className="slideimg">
+                            <img className="eachImg" src="https://picsum.photos/id/237/100/200"/>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
             
         </div>
     )
 }
+
+export default ProductPage
