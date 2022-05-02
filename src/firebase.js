d2b7d396acc3a7b9d89fa1efbd778db47c6b7174
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2022-05-02 03:09:51 +0300

    added configuration and setaup firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,18 @@
+import React from "react";
+import { initializeApp } from 'firebase/app';
+import { collection, getDoc, getFirestore } from 'firebase/firestore';
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCg2BpwgJha6T6TBkrinbwNjdYSZh2HbQ8",
-    authDomain: "arzu-optical.firebaseapp.com",
-    projectId: "arzu-optical",
-    storageBucket: "arzu-optical.appspot.com",
-    messagingSenderId: "163643703465",
-    appId: "1:163643703465:web:11fc209f68a5fff585e9b8",
-    measurementId: "G-E7D61QEBXC"
-  };
+  apiKey: "AIzaSyCg2BpwgJha6T6TBkrinbwNjdYSZh2HbQ8",
+  authDomain: "arzu-optical.firebaseapp.com",
+  databaseURL: "https://arzu-optical-default-rtdb.firebaseio.com",
+  projectId: "arzu-optical",
+  storageBucket: "arzu-optical.appspot.com",
+  messagingSenderId: "163643703465",
+  appId: "1:163643703465:web:11fc209f68a5fff585e9b8",
+  measurementId: "G-E7D61QEBXC"
+};
+const db = initializeApp(firebaseConfig)
+export default getFirestore()
