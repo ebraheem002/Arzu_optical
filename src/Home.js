86720e3bfabb9f1e433841c4e52d738ea810ae95
Ebraheem Mahmood <Ebraheemmahmood991@gmail.com>
2022-05-06 14:24:12 +0300

    rendering products in home page

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,28 @@
-import {React, useEffect, useState} from 'react';
+import React from 'react'
+import { useEffect, useState} from 'react';
 import Product from './Product.js';
-import './Home.css';
-function Home (){
-    const [productsData, setProductsData] = useState([]);
-    const [isLoading, setLoading] = useState(true)
-        useEffect(() => {
-        // GET request using fetch inside useEffect React hook
-        fetch('https://fakestoreapi.com/products')
-        .then(res=>res.json())
-        .then(json=>setProductsData(json))
-      
-      // empty dependency array means this effect will only run once (like componentDidMount in classes)
-      }, []);
+import { ValidApi } from './CartContext.js';
+import { isLoading } from './CartContext.js';
 
-      useEffect(() => {
-          if(productsData.length !==0){
-              setLoading(false);
-              console.log(productsData);
-          }
-      }, [productsData])
+import './Home.css';
+
+function Home (){
+        const {productsData} = ValidApi();
+
+        
+
     return(
         <div className='home'>
         <div className='home_container'>
             
             
             <div className='home_row'>
-                {isLoading ? (
-                    <h1>Loading ....</h1>
-                ): (
-                    productsData.map((myData) =>(
-                        <Product title={myData.title} image={myData.image} price={myData.price} />
+                    {productsData.map((myData) =>(
+
+                        <Product key={myData.id} id={myData.id} title={myData.title} image={myData.image} price={myData.price} />
         
-                    ))
-                )}
+                    ))}
+                )
             
             
 
